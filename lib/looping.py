b486628e12a516a2059cd3960adbfa0fbbaa3060
Felicia Kwame <kwamefelicia2005@gmail.com>
2024-09-01 00:54:14 +0300

    final commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i=10
+    while i>0:
+        print(i)
+        i=i-1
+        print("Happy New Year!")
+        
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list=[1,2,3,4,5,]
+    square_integers = [x**2 for x in int_list]
+    return square_integers
+    
 
 def fizzbuzz():
-    # code goes here!
+    i=0
+    for i in range(1,101):
+    
+        if i%3==0 and i%5==0:
+            print("FizzBuzz")
+        elif i%3==0:
+            print("Fizz")
+        elif i%5==0:
+            print("Buzz")
+        else:
+            print(i)
+    
     pass
